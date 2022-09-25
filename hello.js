c141d022262cb99036f148a018ad0864fec6bbc8
binding1 <jaey.pro@gmail.com>
2022-09-25 17:31:10 +0000

    commiting additional say hello functions

--- hello.js
+++ hello.js
@@ -6,4 +6,22 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
 
+const sayHelloToConsole = function(name){
+   console.log("Hello, " + name);
+}
 
+sayHelloToConsole("John");
+
+const sayHelloToConsole1 = function(name){
+  return ("Hello, " + name);
+}
+
+sayHelloToConsole1("John");
+console.log(sayHelloToConsole1("Jason"))
+
+const returnSayHello = function(name){
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
